2edf11653252ec2c41101b4f553a97028c8bf168
buggy <chandan7@icloud.com>
2015-06-26 09:03:40 -0700

    This is first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by buggy on 6/26/15.
+ */
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send('welcome to my API');
+});
+
+app.listen(port, function () {
+    console.log('running on port:' + port);
+});
