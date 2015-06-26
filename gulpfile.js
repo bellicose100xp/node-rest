5bf60d215f8ed240432cce04e7461079f15f13a3
buggy <chandan7@icloud.com>
2015-06-26 09:18:49 -0700

    added gulp file & nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,19 @@
 /**
  * Created by buggy on 6/26/15.
  */
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon');
+
+gulp.task('default', function () {
+   nodemon({
+       script: 'app.js',
+       ext: 'js',
+       env: {
+           PORT: 8000
+       },
+       ignore: ['./mode_modules/**']
+   })
+       .on('restart', function () {
+           console.log('Restarting.....');
+       });
+});
